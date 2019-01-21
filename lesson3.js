7f29a24ffc9ce87909f19e744d363a0cafae2203
Виталий <Vetal1402@mail.ru>
2019-01-21 16:12:59 +0600

    Задание третьего урока

---
+++ lesson3.js
@@ -0,0 +1,45 @@
+console.log("Ответ на пeрвый вопрос");
+let arr1 = [];
+let a = 0;
+
+while(a<100){
+    a++;
+    arr1.push(a);
+}
+console.log(arr1);
+
+console.log('Ответ на второй вопрос');
+let b = 0;
+
+do{
+
+
+    if(b === 0){
+        console.log(b +' - это ноль');
+
+    }
+    if(b % 2 === 0){
+        console.log(b + ' - это четное');
+    }
+    else{
+        console.log(b + ' - это нечетное');
+    }
+    b++;
+}
+
+while(b <= 10);
+
+console.log('Ответ на третий вопрос');
+
+let i;
+for(i = 0; i <= 9; i++){
+    console.log(i);
+}
+
+var arr3 = [];
+var end = 0;
+while (end < 20){
+    end++;
+    arr3.push('x');
+    console.log(arr3);
+}
